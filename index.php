e507421f0afe90b268f041fdedfca9df1adccb33
Ricky Sandoval <rsandoval@arrow.com>
2016-01-27 20:59:59 -0700

    move analytics to head

--- index.php
+++ index.php
@@ -5,6 +5,17 @@
 	<title>Ricky Sandoval Web Development</title>
 	<link rel="stylesheet" href="assets/css/reset.css">
 	<link rel="stylesheet" href="assets/css/styles.css">
+	<!-- Google analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-64191814-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>
 <body>
 	<div id="page">
@@ -334,14 +345,4 @@ if (isset($_POST['submit'])) {
 	</div>
 	<script src="assets/js/jquery.min.js"></script>
 	<script src="assets/js/min/script-min.js"></script>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-64191814-1', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
 </body>
