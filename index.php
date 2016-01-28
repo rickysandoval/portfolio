620cfa9215988ab4bc865de8184c8253f37d3856
Ricky Sandoval <rsandoval@arrow.com>
2016-01-27 20:56:59 -0700

    Add google analytics tracking code

--- index.php
+++ index.php
@@ -334,4 +334,14 @@ if (isset($_POST['submit'])) {
 	</div>
 	<script src="assets/js/jquery.min.js"></script>
 	<script src="assets/js/min/script-min.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-64191814-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </body>
